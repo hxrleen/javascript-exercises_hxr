c608c7fe78083122ec0ac320e1a959dba456b698
hxrleen <rini571raniyal@gmail.com>
2023-08-29 19:47:27 +0530

    calculator.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function (a, b) {
+  var result = parseInt(a) + parseInt(b);
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  var result = parseInt(a) - parseInt(b);
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  var result = 0;
+  for (i = 0; i < arr.length; i++) {
+    result += parseFloat(arr[i]);
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  var sum = 1;
+  for (var i = 0; i < arr.length; i++) {
+    sum = sum * parseInt(arr[i]);
+  }
+  return sum;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  var result = parseInt(a ** b);
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  var i;
+  var sum = 1;
+
+  for (i = 1; i <= parseInt(num); i++) {
+    sum = parseInt(sum * i);
+  }
+  return sum;
 };
 
 // Do not edit below this line
@@ -29,5 +46,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
