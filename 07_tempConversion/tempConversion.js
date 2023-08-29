8db6e03c6f94e01fbdfa747a536f94b75eaa08d8
hxrleen <rini571raniyal@gmail.com>
2023-08-29 18:45:12 +0530

    tempConversion.js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  var f = parseInt(fahrenheit);
+  var c = (f - 32) * (5 / 9);
+  var c = Math.round(c * 10) / 10;
+  return c;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  var c = parseFloat(celsius);
+  var f = parseFloat((9 / 5) * c + 32);
+  var f = Math.round(f * 10) / 10;
+
+  return f;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
