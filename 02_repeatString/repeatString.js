77366bdb4e96c6dc6c6972251c28fe73e1106057
hxrleen <rini571raniyal@gmail.com>
2023-08-29 16:45:26 +0530

    repeatString.spec.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function (word, times) {
+  let string = "";
+  if (times == 0) {
+    return "";
+  } else if (times < 0) {
+    return "ERROR";
+  } else if (word == "") {
+    return "";
+  } else {
+    for (var i = 0; i < times; i++) {
+      string += word;
+    }
+  }
 
+  return string;
 };
 
 // Do not edit below this line
