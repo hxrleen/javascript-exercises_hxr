922a2ce6d91b1bacf85635b6b31329b1d76ef118
hxrleen <rini571raniyal@gmail.com>
2023-08-29 16:52:14 +0530

    reverseString.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let word = "";
+  let num = string.length;
+  let arr = string.split("");
+  for (var i = num - 1; i >= 0; i--) {
+    word = word + arr[i];
+  }
+  return word;
 };
 
 // Do not edit below this line
