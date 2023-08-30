19da3706bc671b7fe2f6b1dbd71c5f71f9d858d5
hxrleen <rini571raniyal@gmail.com>
2023-08-30 10:33:16 +0530

    palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const cleanedString = string.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+  const len = cleanedString.length;
+  let result = "";
 
+  for (let i = len - 1; i >= 0; i--) {
+    result += cleanedString[i];
+  }
+
+  return result === cleanedString;
 };
 
 // Do not edit below this line
