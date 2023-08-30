cc72ff0b05cc234c783c1e6efd1c1b6ca309c89a
hxrleen <rini571raniyal@gmail.com>
2023-08-30 15:03:06 +0530

    getTheTitles.js

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (books) {
+  let newarr = [];
+  for (let i = 0; i < books.length; i++) {
+    newarr.push(books[i].title);
+  }
+  return newarr;
 };
 
 // Do not edit below this line
