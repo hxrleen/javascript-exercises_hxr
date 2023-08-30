773efc6e912be363ab96c5475fef7565b28e76d2
hxrleen <rini571raniyal@gmail.com>
2023-08-30 11:17:12 +0530

    fibonacci.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  if (num < 0) {
+    return "OOPS";
+  } else {
+    const arr = [1, 1];
+    for (let i = 2; i < num; i++) {
+      const element = arr[i - 1] + arr[i - 2];
+      arr.push(element);
+    }
+    return arr[arr.length - 1];
+  }
 };
 
 // Do not edit below this line
