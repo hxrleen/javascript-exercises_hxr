3d9c71772dc127995beacffcaf77e8f2be4771d7
hxrleen <rini571raniyal@gmail.com>
2023-08-30 16:47:53 +0530

    12_findTheOldest/findTheOldest.js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  let oldestName = "";
+  let maxAge = -1;
 
+  for (let i = 0; i < people.length; i++) {
+    const currentYear = new Date().getFullYear();
+    const age = people[i].yearOfDeath
+      ? people[i].yearOfDeath - people[i].yearOfBirth
+      : currentYear - people[i].yearOfBirth;
+
+    if (age > maxAge) {
+      maxAge = age;
+      oldestName = people[i].name;
+    }
+  }
+
+  return { name: oldestName, age: maxAge };
 };
 
 // Do not edit below this line
